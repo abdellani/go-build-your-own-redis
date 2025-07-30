5da26c51467a48d3a512554ea138162ef187cf2c
Abdellani <abdellani@gmail.com>
2025-07-30 17:44:34 +0100

    support adding multiple items at once (RPUSH)

--- app/handler/rpush.go
+++ app/handler/rpush.go
@@ -6,7 +6,11 @@ import (
 
 func (h *Handler) RPush(cmd *deserializer.Command) string {
 	key := cmd.Arguments[0]
-	value := cmd.Arguments[1]
-	size := h.Storage.Push(key, value)
+	items := len(cmd.Arguments)
+	size := 0
+	for i := 1; i < items; i++ {
+		value := cmd.Arguments[i]
+		size = h.Storage.Push(key, value)
+	}
 	return h.Serializer.Integer(size)
 }
